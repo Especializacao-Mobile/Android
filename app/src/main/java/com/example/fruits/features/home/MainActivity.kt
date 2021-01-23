47d255b362dce6a74cd8bb910beb21e9d1fadc81
ironman <daivid.v.leal@gmail.com>
2021-01-23 15:31:44 -0300

    init part 2

--- app/src/main/java/com/example/fruits/features/home/MainActivity.kt
+++ app/src/main/java/com/example/fruits/features/home/MainActivity.kt
@@ -4,26 +4,35 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.SwitchCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fruits.R
 import com.example.fruits.databinding.ActivityMainBinding
+import com.example.fruits.databinding.ToolbarBinding
 import com.example.fruits.features.addfruit.AddFruitActivity
 import com.example.fruits.features.fruitdetails.FruitDetailsActivity
 import com.example.fruits.features.fruitdetails.FruitDetailsActivity.Companion.FRUIT
 import com.example.fruits.features.home.adapter.FruitAdapter
 import com.example.fruits.model.Fruit
+import com.example.fruits.model.mockedFruits
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbarBinding: ToolbarBinding
     private lateinit var fruits: ArrayList<Fruit>
     private lateinit var fruitAdapter: FruitAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        toolbarBinding = ToolbarBinding.bind(binding.root)
         setContentView(binding.root)
+        setSupportActionBar(toolbarBinding.toolbar)
         setUpRecyclerView()
         setUpFloatListener()
     }
@@ -35,7 +44,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        fruits.addAll(savedInstanceState.getParcelableArrayList(MAIN_LIST_STATE) ?: ArrayList())
+        fruits.clear()
+        fruits.addAll(savedInstanceState.getParcelableArrayList(MAIN_LIST_STATE) ?: mockedFruits.initialFruits)
         fruitAdapter.notifyDataSetChanged()
     }
 
@@ -50,6 +60,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_filter -> {
+            val builder = AlertDialog.Builder(this)
+            val view = layoutInflater.inflate(R.layout.custom_dialog, null)
+
+            builder.apply {
+                setView(view)
+                setPositiveButton("Filter") { dialog, _ ->
+
+                    dialog.dismiss()
+                }
+            }
+
+            builder.create().show()
+            true
+        }
+
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
     private fun setUpFloatListener() {
         binding.addFruit.setOnClickListener {
             startActivityForResult(
@@ -61,6 +100,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setUpRecyclerView() {
         fruits = ArrayList()
+        fruits.addAll(mockedFruits.initialFruits)
         fruitAdapter = FruitAdapter(fruits, ::fruitDetails)
         binding.fruits.adapter = fruitAdapter
         binding.fruits.layoutManager = LinearLayoutManager(this)
