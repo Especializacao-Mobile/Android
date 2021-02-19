d920daf2ca81857d533a2cc0046df7b6f78b70a4
daivid.v.leal <daivid.v.leal@gmail.com>
2021-02-19 20:25:00 -0300

    fix permissions

--- app/src/main/java/com/example/galleryexercise/feature/home/MainActivity.kt
+++ app/src/main/java/com/example/galleryexercise/feature/home/MainActivity.kt
@@ -29,27 +29,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        requestRead()
+        requestPermissions()
         setUpListeners()
     }
 
-    private fun setUpListeners(){
+    private fun setUpListeners() {
         binding.btnRequestPermission.setOnClickListener {
-            requestRead()
-            requestWrite()
+            requestPermissions()
         }
     }
 
     override fun onResume() {
-        requestRead()
-        requestWrite()
+        requestPermissions()
         super.onResume()
     }
 
     private fun setUpRecyclerView() {
         switchVisibilities(true)
         images = getImages() as ArrayList<Image>
-        imagesAdapter = GalleryAdapter(images){
+        imagesAdapter = GalleryAdapter(images) {
             val intent = Intent(this, DeleteActivity::class.java)
             intent.putExtra(DeleteActivity.IMAGE, it)
             startActivity(intent)
@@ -65,37 +63,41 @@ class MainActivity : AppCompatActivity() {
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        images.addAll((savedInstanceState.getSerializable(MAIN_LIST_STATE) as? ArrayList<Image>)
-                ?: ArrayList())
+        images.addAll(
+            (savedInstanceState.getSerializable(MAIN_LIST_STATE) as? ArrayList<Image>)
+                ?: ArrayList()
+        )
         imagesAdapter.notifyDataSetChanged()
     }
-    
+
     companion object {
         const val MAIN_LIST_STATE = "MAIN_LIST_STATE"
-        const val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1
-        const val MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1
+        const val MY_PERMISSIONS_REQUEST_READ_WRITE_EXTERNAL_STORAGE = 1
     }
 
     private fun getImages(): List<Image> {
         val list = mutableListOf<Image>()
         val projection = arrayOf(
-                MediaStore.Images.ImageColumns._ID,
-                MediaStore.Images.ImageColumns.DISPLAY_NAME
+            MediaStore.Images.ImageColumns._ID,
+            MediaStore.Images.ImageColumns.DISPLAY_NAME
         )
         contentResolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                projection, null, null, null
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection, null, null, null
         ).use {
             it?.let { cursor ->
                 val idColumn = cursor.getColumnIndexOrThrow(
-                        MediaStore.Images.ImageColumns._ID)
+                    MediaStore.Images.ImageColumns._ID
+                )
                 val displayNameColumn = cursor.getColumnIndexOrThrow(
-                        MediaStore.Images.ImageColumns.DISPLAY_NAME)
-                while(cursor.moveToNext()) {
+                    MediaStore.Images.ImageColumns.DISPLAY_NAME
+                )
+                while (cursor.moveToNext()) {
                     val id = cursor.getLong(idColumn)
                     val displayName = cursor.getString(displayNameColumn)
                     val contentUri = ContentUris.withAppendedId(
-                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id
+                    )
                     list.add(Image(id, displayName, contentUri))
                 }
             }
@@ -108,44 +110,48 @@ class MainActivity : AppCompatActivity() {
         binding.btnRequestPermission.isVisible = !galleryVisible
     }
 
-    private fun requestRead() {
-        if (ContextCompat.checkSelfPermission(this,
-                        Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
+    private fun requestPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
+                MY_PERMISSIONS_REQUEST_READ_WRITE_EXTERNAL_STORAGE
             )
         } else {
             setUpRecyclerView()
         }
     }
 
-    private fun requestWrite() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE
-            )
-        } else {
-            setUpRecyclerView()
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (requestCode == MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                setUpRecyclerView()
-            } else {
-                Toast.makeText(this, getString(R.string.access_denied), Toast.LENGTH_SHORT).show()
-                switchVisibilities()
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_READ_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults.isNotEmpty()) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    setUpRecyclerView()
+                } else {
+                    Toast.makeText(this, getString(R.string.access_denied), Toast.LENGTH_SHORT)
+                        .show()
+                    switchVisibilities()
+                }
             }
-            return
-        }
-        if (requestCode == MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE) {
-            Toast.makeText(this, getString(R.string.not_possible_to_delete) , Toast.LENGTH_SHORT).show()
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
-    
+
 }
