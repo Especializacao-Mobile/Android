deea8667bb3f4e6d84c12c3700d712e897f69d84
daivid.v.leal <daivid.v.leal@gmail.com>
2021-02-19 18:42:59 -0300

    layout created

--- app/src/main/java/com/example/galleryexercise/GalleryAdapter.kt
+++ app/src/main/java/com/example/galleryexercise/GalleryAdapter.kt
@@ -1,20 +1,25 @@
 package com.example.galleryexercise
 
 import android.provider.MediaStore
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.galleryexercise.databinding.GalleryItemBinding
 
 class GalleryAdapter(private val items: List<MediaStore.Images>) :
     RecyclerView.Adapter<GalleyItemViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleyItemViewHolder {
+    private lateinit var binding: GalleryItemBinding
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleyItemViewHolder {
+        binding = GalleryItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return GalleyItemViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: GalleyItemViewHolder, position: Int) {
-        holder.bind()
+        holder.bind(items[position])
     }
 
-    override fun getItemCount() =items.size
+    override fun getItemCount() = items.size
 
 }
--- app/src/main/java/com/example/galleryexercise/GalleyItemViewHolder.kt
+++ app/src/main/java/com/example/galleryexercise/GalleyItemViewHolder.kt
@@ -1,12 +1,13 @@
 package com.example.galleryexercise
 
+import android.provider.MediaStore
 import androidx.recyclerview.widget.RecyclerView
 import com.example.galleryexercise.databinding.GalleryItemBinding
 
 class GalleyItemViewHolder(private val binding: GalleryItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(){
+    fun bind(item: MediaStore.Images) {
 
     }
 }
